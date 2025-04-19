a5db0e4030e77ddb13a1ac102bd3fe8037cc3da3
malikobansa <hackobansa@gmail.com>
2025-04-19 01:35:59 +0100

    added code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script"; // Import the Script component
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
